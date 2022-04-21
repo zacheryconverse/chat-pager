8a16f9e29d99b15a67c2240f5e3359d73abd054b
Zachery Converse <zacheryconverse@gmail.com>
2022-04-20 19:08:24 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelContainer, ChannelListContainer } from "./components";
+
 import './App.css';
 
-function App() {
+const apiKey = "86nn7ccc5364";
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client={client} theme="dark">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
-}
+};
 
 export default App;
